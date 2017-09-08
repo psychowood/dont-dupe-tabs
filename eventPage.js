09627419c4eae1f30cb67228c9ced6cf50e62840
psychowood <psychowood@gmail.com>
2017-09-08 23:43:33 +0200

    First version

---
+++ eventPage.js
@@ -0,0 +1,82 @@
+(function () {
+    console.log("started");
+
+    var tabsQueue = {};
+
+    function updateIcon(enabled) {
+        if (enabled) {
+            chrome.browserAction.setTitle({ title: 'Enabled' });
+            chrome.browserAction.setIcon({ path: 'icon-open.png' });
+        } else {
+            chrome.browserAction.setTitle({ title: 'Disabled' });
+            chrome.browserAction.setIcon({ path: 'icon-closed.png' });
+        }
+    };
+
+    document.addEventListener("DOMContentLoaded", function () {
+        console.log("onLoad");
+        tabsQueue = {};
+        chrome.storage.sync.get({ 'enabled': true }, function (options) {
+            var enabled = options.enabled === true;
+            chrome.storage.sync.set({ 'enabled': enabled }, function () {
+                updateIcon(enabled);
+            });
+        });
+    }, false);
+
+    chrome.tabs.onCreated.addListener(function (tab) {
+        var tslog = new Date().getTime();
+        console.log(tslog, "onCreated", tab.id);
+        var tabUrl = tab.url;
+        console.log(tslog, "storage get");
+        chrome.storage.sync.get({ 'enabled': true }, function (options) {
+            console.log(tslog, "storage got");
+            var enabled = (options.enabled === true);
+            updateIcon(enabled);
+            if (enabled) {
+                if (!tabsQueue[tabUrl]) {
+                    console.log(tslog, "url not found", tabUrl);
+                    tabsQueue[tabUrl] = true;
+                    var tabId = tab.id;
+                    chrome.browserAction.setBadgeText({ text: '' });
+                    chrome.browserAction.setBadgeBackgroundColor({ color: '#618cae' });
+                    console.log(tslog, "enabled, querying");
+                    chrome.tabs.query({ url: tabUrl }, function (tabs) {
+                        console.log(tslog, "query result", tabs);
+                        if (tabs) {
+                            let i, t, removed = 0;
+                            for (i = 0; i < tabs.length; i++) {
+                                t = tabs[i];
+                                if (t.id !== tabId) {
+                                    chrome.tabs.remove(t.id);
+                                    console.log(tslog, "duplicate found, removing", t.id);
+                                    removed++;
+                                }
+                            }
+                            if (removed > 0) {
+                                chrome.browserAction.setBadgeText({ text: '' + removed });
+                                chrome.browserAction.setTitle({ title: 'Closed ' + removed + ' duplicated tabs' });
+                            }
+                        }
+                        delete tabsQueue[tabUrl];
+                    });
+                } else {
+                    console.log(tslog, "url aready queued", tabUrl);
+                }
+            } else {
+                console.log(tslog, "extension disabled");
+            };
+        });
+    });
+
+    chrome.browserAction.onClicked.addListener(function (tabs) {
+        console.log("onClicked");
+        chrome.browserAction.setBadgeText({ text: '' });
+        chrome.storage.sync.get({ 'enabled': true }, function (olditems) {
+            var enabled = olditems.enabled !== true;
+            chrome.storage.sync.set({ 'enabled': enabled }, function () {
+                updateIcon(enabled);
+            });
+        });
+    });
+})();
